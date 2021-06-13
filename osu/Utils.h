d96d6c43d8a16ec4263cd7c3dde9b397dab0288b
adrianjch <adrianjimenezchincolla@gmail.com>
2021-06-13 04:56:41 +0200

    Removed useless code, made a couple of improves in this shit code lol

--- osu/Utils.h
+++ osu/Utils.h
@@ -1,20 +1,21 @@
 #pragma once
-#include<Windows.h>
+#include <Windows.h>
 #include <string>
 #include <fstream>
+#include <deque>
 //limits -> 0, 0 = 546, 273
 //limits - > 512, 384 = 989, 605
 
-enum Type{CIRCLE, SLIDER, SPINNER, NOTHING};
+enum Type { CIRCLE, SLIDER, SPINNER, NOTHING };
 struct Object {
 	Type type;
 	float x;
 	float y;
 	int timer;
-	int timer2;
-	
+	int spinnerLength;
 
-	void setObjectType(const std::string &content) {
+
+	void setObjectType(const std::string& content) {
 		type = NOTHING;
 		int counter = 0;
 		for (int i = 0; i < content.size(); i++) {
@@ -57,7 +58,7 @@ struct Object {
 	}
 };
 
-std::ostream& operator<<(std::ostream &os, const Type &ob) {
+std::ostream& operator<<(std::ostream& os, const Type& ob) {
 	if (ob == CIRCLE)
 		os << "circle";
 	else if (ob == SLIDER)
@@ -68,6 +69,54 @@ std::ostream& operator<<(std::ostream &os, const Type &ob) {
 		os << "nothing";
 	os << '\n';
 	return os;
+}
+
+struct vec2 {
+	float x, y;
+	vec2(float _x, float _y) {
+		x = _x;
+		y = _y;
+	}
+	vec2() {
+		x = 0;
+		y = 0;
+	}
+};
+
+vec2 operator+(vec2 a, vec2 b) {
+	return vec2(a.x + b.x, a.y + b.y);
+}
+
+vec2 operator-(vec2 a, vec2 b) {
+	return vec2(a.x - b.x, a.y - b.y);
+}
+
+vec2 operator*(float s, vec2 a) {
+	return vec2(s * a.x, s * a.y);
+}
+
+
+vec2 getBezierPoint(vec2* points, int numPoints, float t) {
+	vec2* tmp = new vec2[numPoints];
+	memcpy(tmp, points, numPoints * sizeof(vec2));
+	int i = numPoints - 1;
+	while (i > 0) {
+		for (int k = 0; k < i; k++)
+			tmp[k] = tmp[k] + t * (tmp[k + 1] - tmp[k]);
+		i--;
+	}
+	vec2 answer = tmp[0];
+	delete[] tmp;
+	return answer;
+}
+vec2 getBezierPoint2(const std::deque<vec2>& points, const float& t) {
+	std::deque<vec2> tmp(points);
+	vec2 answer;
+	for (int i = points.size() - 1; i > 0; i--) {
+		for (int k = 0; k < i; k++)
+			tmp[k] = tmp[k] + t * (tmp[k + 1] - tmp[k]);
+	}
+	return tmp[0];
 }
 
 void leftClick() {
@@ -96,7 +145,27 @@ void rightClick() {
 	Input.mi.dwFlags = MOUSEEVENTF_RIGHTUP;
 	::SendInput(1, &Input, sizeof(INPUT));
 }
+void hold() {
+	INPUT    Input = { 0 };
+	// left down 
+	Input.type = INPUT_MOUSE;
+	Input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+	::SendInput(1, &Input, sizeof(INPUT));
+
+}
+void release() {
+	INPUT    Input = { 0 };
+	// left up
+	::ZeroMemory(&Input, sizeof(INPUT));
+	Input.type = INPUT_MOUSE;
+	Input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+	::SendInput(1, &Input, sizeof(INPUT));
+}
 void moveMouse(int x, int y) {
+	// Resolution change TO-DO
+	x = (x * 0.86f) + 546;
+	y = (y * 0.86f) + 273;
+
 	double fScreenWidth = ::GetSystemMetrics(SM_CXSCREEN) - 1;
 	double fScreenHeight = ::GetSystemMetrics(SM_CYSCREEN) - 1;
 	double fx = x * (65535.0f / fScreenWidth);
@@ -111,12 +180,13 @@ void moveMouse(int x, int y) {
 
 std::string changeSong() {
 	std::string song;
-	std::ifstream songFile("D:/Juegos/StreamCompanion/Files/osuFileLocation.txt");
+	std::ifstream songFile("D:/ppCounter/StreamCompanion/Files/osuFileLocation.txt");
 	std::getline(songFile, song);
 	for (int i = 0; i < song.size(); i++) {
-		if (song[i] == 92)
+		if (song[i] == '\\')
 			song[i] = '/';
 	}
 	songFile.close();
 	return song;
 }
+
--- osu/main.cpp
+++ osu/main.cpp
@@ -3,11 +3,16 @@
 #include <string>
 #include <iostream>
 #include <chrono>
+#include <math.h>
 
 int main() {
-	float x = 400;
-	float y = 300;
+	const double PI = 3.14159265;
+	const float speed = 10.f;//3.2f
+	float mouseX = 400;
+	float mouseY = 300;
+	float timeMultiplier = 1; //0.75 for HT and 1.5 for DT
 	int timer = 0;
+	int timer2;
 	int firstTimer;
 	bool firstTime;
 	int aux;
@@ -15,26 +20,26 @@ int main() {
 	std::string song;
 	std::string content = "";
 	Object object;
-	//moveMouse(308,101);
-	//moveMouse(1229,791);
+	/*moveMouse(308,101);
+	moveMouse(1229,791);*/
 	while (true) {
 		///press enter to change to current song
 		if (GetAsyncKeyState(VK_RETURN)) {
 			song = changeSong();
 		}
-		///press c to start
-		if (GetAsyncKeyState(0x43)) {
+		///press 'Z' to start
+		if (GetAsyncKeyState(0x5A)) {
 			firstTime = true;
-			//starts reading the file
+			///starts reading the file
 			std::ifstream file(song);
 			while (content != "[HitObjects]")
 				std::getline(file, content);
 			content = "";
 			std::noskipws(file);
 			std::chrono::steady_clock::time_point start = std::chrono::high_resolution_clock::now();
-			//starts reading the objects
+			///starts reading the objects
 			while (!file.eof()) {
-				//set object coords+timer
+				///set object coords+timer
 				file >> object.x;
 				if (file.eof())
 					break;
@@ -44,61 +49,114 @@ int main() {
 				file >> object.timer;
 				if (firstTime) {
 					firstTimer = object.timer;
+					timer2 = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count();
 					firstTime = false;
 				}
 				file.ignore();
 				file >> aux;
-				if (aux == 12) {
+				if (aux == 2) {///if is a slider
+					/*file.ignore();
+					file >> aux;
+					file.ignore();
+					file.ignore();*/
+					object.type = SLIDER;
+				}
+				else if (aux == 12) {///if is a spinner
 					file.ignore();
 					file >> aux;
 					file.ignore();
-					file >> object.timer2;
+					file >> object.spinnerLength;
+					object.type = SPINNER;
 				}
-				//set object type
+				else
+					object.type = CIRCLE;
+				///set object type
 				std::getline(file, content);
-				object.setObjectType(content);
+				/*object.setObjectType(content);*/
+				///CIRCLE
 				if (object.type == CIRCLE || object.type == SLIDER) {
-					while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count() < object.timer - firstTimer) {
+					float incrementerX = (object.x - mouseX) / ((object.timer - timer2) / timeMultiplier);
+					float incrementerY = (object.y - mouseY) / ((object.timer - timer2) / timeMultiplier);
+					while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count() < (object.timer - firstTimer) / timeMultiplier) {
+						Sleep(2);
+						if (incrementerX > 0 && mouseX < object.x) {
+							mouseX += incrementerX * speed;
+							mouseY += incrementerY * speed;
+							moveMouse(mouseX, mouseY);
+						}
+						else if (incrementerX < 0 && mouseX > object.x) {
+							mouseX += incrementerX * speed;
+							mouseY += incrementerY * speed;
+							moveMouse(mouseX, mouseY);
+						}
+						else if (incrementerX == 0) {
+							if (incrementerY > 0 && mouseY < object.y) {
+								mouseX += incrementerX * speed;
+								mouseY += incrementerY * speed;
+								moveMouse(mouseX, mouseY);
+							}
+							else if (incrementerY < 0 && mouseY > object.y) {
+								mouseX += incrementerX * speed;
+								mouseY += incrementerY * speed;
+								moveMouse(mouseX, mouseY);
+							}
+						}
+
 
 					}
-					moveMouse((object.x*0.86) + 546, (object.y*0.86) + 273);
+					moveMouse(mouseX, mouseY);
+					rightClick();
+					timer2 = object.timer;
+					mouseX = object.x;
+					mouseY = object.y;
 				}
+				///SLIDER
+				else if (object.type == SLIDER) {
+					while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count() < (object.timer - firstTimer) / timeMultiplier) {
+
+					}
+					/*hold();
+					std::deque<vec2> tmp;
+					tmp.push_back({ 144,89 });
+					tmp.push_back({ 271,88 });
+					tmp.push_back({ 337,147 });
+					tmp.push_back({ 215,243 });
+					tmp.push_back({ 206,349 });
+					tmp.push_back({ 474,228 });
+					vec2 aux;
+					for (float i = 0; i <= 1; i += 0.02f/((455/100)/1.4f)) {
+						Sleep(20);
+						aux = getBezierPoint2(tmp, i);
+						moveMouse(aux.x*0.86 + 546, aux.y*0.86 + 273);
+					}
+					while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count() < object.timer - firstTimer + 455*10/1.4f + 20) {
+
+					}
+					release();*/
+				}
+				///SPINNER
 				else if (object.type == SPINNER) {
 					std::chrono::steady_clock::time_point temp = std::chrono::high_resolution_clock::now();
-					int phase = 0;
-					bool move = true;
-					while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count() < object.timer2 - firstTimer) {
-						while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count() < object.timer - firstTimer) {
+					int angle = 0;
+					hold();
+					while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count() < (object.spinnerLength - firstTimer) / timeMultiplier) {
+						while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count() < (object.timer - firstTimer) / timeMultiplier) {
 
 						}
-						if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - temp).count() > 30) {
-							phase++;
-							phase = phase % 4;
+						if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - temp).count() > 2) {
+							moveMouse(256 + 50 * cos(angle * PI / 180), 192 + 50 * sin(angle * PI / 180));
 							temp = std::chrono::high_resolution_clock::now();
-							move = true;
-						}
-
-						if (phase == 0 && move) {
-							moveMouse((226 * 0.86) + 546, (162 * 0.86) + 273);
-							move = false;
-						}
-						else if (phase == 1 && move) {
-							moveMouse((286 * 0.86) + 546, (162 * 0.86) + 273);
-							move = false;
-						}
-						else if (phase == 2 && move) {
-							moveMouse((286 * 0.86) + 546, (222 * 0.86) + 273);
-							move = false;
-						}
-						else if (phase == 3 && move) {
-							moveMouse((226 * 0.86) + 546, (222 * 0.86) + 273);
-							move = false;
+							angle += 15;
+							angle = angle % 360;
 						}
 					}
+					release();
+					timer2 = object.spinnerLength;
+					mouseX = object.x;
+					mouseY = object.y;
 				}
-				//std::cout << object.type;
-				///press numpad 5 to stop
-				if (GetAsyncKeyState(VK_NUMPAD5))
+				///press ESCAPE to stop
+				if (GetAsyncKeyState(VK_ESCAPE))
 					break;
 			}
 			file.close();
